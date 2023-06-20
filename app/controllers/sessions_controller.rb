0349ede3ac84ed2cbb9fe65ef4b1491130197408
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-06-21 00:15:58 +0900

    ログイン回りを実装(viewはまだ)、新規登録回りも実装(新規登録view作成)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+
   def new
   end
 
   def create
+    # まず、送られてきたメースアドレスでユーザーを検索する
+    user = User.find_by(email: session_params[:email])
+
+      # ユーザーが見つかった場合、送られてきたパスワードによる認証をauthenticateメソッドを使って行う
+      if user&.authenticate(session_params[:password])
+        # 認証に成功した場合に、セッションにuser_idを格納しています。
+        session[:user_id]=user.id
+
+        redirect_to root_path, notice: "ログインしました"
+
+      else
+        render :new
+      end
   end
 
   def destroy
+    reset_session
+    redirect_to root_path, notice: "ログアウトしました"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ユーザー登録・退会
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'profile'
+  delete '/users/:id', to: 'users#destroy', as: 'unsubscribe'
+
+  # ログイン・ログアウト
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
