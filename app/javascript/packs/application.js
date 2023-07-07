eb3fec3b0a6d3226b624593c901b08b460a2ea5e
fukumura <shunshun2967@gmail.com>
2023-07-07 09:23:52 -0700

    fix-config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery3/dist/jquery"
-import "popper.js/dist/popper"
-import "bootstrap/dist/js/bootstrap"
+// import "jquery3/dist/jquery"
+// import "popper.js/dist/popper"
+// import "bootstrap/dist/js/bootstrap"
 
 Rails.start()
 Turbolinks.start()
