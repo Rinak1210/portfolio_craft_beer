6accb9c30e82b46caf995d8d31a0ff690140cd9a
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-07-17 22:48:38 +0900

    コメント機能を実装（非同期処理についてエラーが出ているので、そちらは未対応）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,30 @@
 class CommentsController < ApplicationController
+  # Ajax通信用
   def create
+    @craft_beer_shop = CraftBeerShop.find(params[:craft_beer_shop_id])
+    # 現在のユーザーに紐づいたコメントを作成
+    @comment = current_user.comments.new(comment_params)
+    @comment.craft_beer_shop_id = @craft_beer_shop.id
+    if @comment.save
+      flash.now[:notice] = 'コメントを投稿しました'
+      render :comment
+    else
+      render :error
+    end
   end
 
+  # Ajax通信用
   def destroy
+    Comment.find_by(params[:id], craft_beer_shop_id: params[:craft_beer_shop_id]).destroy
+    flash.now[:alert] = '投稿を削除しました'
+    #renderしたときに@craft_beer_shopのデータがないので@postを定義
+    @craft_beer_shop = CraftBeerShop.find(params[:craft_beer_shop_id])
+    render :comment  #render先にjsファイルを指定
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
