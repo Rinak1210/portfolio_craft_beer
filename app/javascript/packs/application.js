8ed19229fe3a91f980c88fe3eb98318374dcc6dd
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-08-04 01:33:39 +0900

    ハンバーガーメニューが出現するように改善

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,23 @@
-import "jquery"
-import '@fortawesome/fontawesome-free/js/all';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-// import "jquery3/dist/jquery"
-// import "popper.js/dist/popper"
-// import "bootstrap/dist/js/bootstrap"
+import "jquery"
+import '@fortawesome/fontawesome-free/js/all';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", function() {
+
+  // For Bootstrap tooltips and popovers
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+
+  // For handling Bootstrap navbar toggler
+  $('.navbar-toggler').on('click', function () {
+    $('.navbar-collapse').toggleClass('show');
+  });
+});
+
