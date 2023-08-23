8b9497affbdb7c846efddfebb54f64afa6be27f3
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-08-23 23:40:37 +0900

    デザインの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :craft_beer_shop
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 30 }
 end
